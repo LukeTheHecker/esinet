4f0d0d79d329dacfb09b7121ce7991ca4a1d69d0
unknown <lukas_hecker@web.de>
2021-01-19 20:06:57 +0100

    setup stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+Solve the M/EEG inverse problem using artificial neural networks with Python 3
+and the MNE library.
+
+The functions allow you to 
+* Create a forward model that solves the M/EEG forward problem
+* Simulate brain sources and corresponding EEG activity
+* Load and train a neural network to solve the inverse problem
+* Inspect the performance of the neural network* 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ESINet", # Replace with your own username
+    version="0.0.1",
+    author="Lukas Hecker",
+    author_email="lukas_hecker@web.de",
+    description="Solve the M/EEG inverse problem using artificial neural networks with Python 3 and the MNE library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LukeTheHecker/ESINet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
