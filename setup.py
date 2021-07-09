c4321dfbbc5be40f037620fc28c71a16d8cf77e3
Lukas <lukas_hecker@web.de>
2021-07-09 15:54:50 +0200

    new lower cap spelling

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ESINet",
-    version="0.0.12",
+    name="esinet",
+    version="0.0.13",
     author="Lukas Hecker",
     author_email="lukas_hecker@web.de",
     description="Solve the M/EEG inverse problem using artificial neural networks with Python 3 and the MNE library.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/LukeTheHecker/ESINet",
+    url="https://github.com/LukeTheHecker/esinet",
     packages=setuptools.find_packages(),
     install_requires=['tensorflow>=2.4.1','mne>=0.22.0', 'scipy', 'colorednoise', 'joblib', 'jupyter', 'ipykernel', 'matplotlib', 'pyvista>=0.27.4', 'pyvistaqt>=0.3.0', 'vtk>=9.0.1', 'tqdm'],
     classifiers=[
--- tst.py
+++
@@ -1,2 +0,0 @@
-from ESINet.tests import run_unit_tests
-run_unit_tests()
