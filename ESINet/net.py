fixed scaling to pos and neg, bug fixes wrt temporal model, fixed loss for negative valued y_true

ESINet/net.py
@@ -34,7 +34,7 @@ class Net(keras.Sequential):
     evaluate : evaluate the performance of the model
     '''
     
-    def __init__(self, fwd, n_layers=1, n_neurons=128, n_lstm_units=250, 
+    def __init__(self, fwd, n_layers=1, n_neurons=128, n_lstm_units=100, 
         activation_function='swish', n_jobs=-1, verbose=False):
 
         super().__init__()
@@ -213,7 +213,7 @@ class Net(keras.Sequential):
         else:
             x_scaled = np.squeeze(x_scaled)
             y_scaled = np.squeeze(y_scaled)
-        print(x_scaled.shape, y_scaled.shape)
+        print(x_scaled.shape, y_scaled.shape, y_scaled.max(), y_scaled.min())
 
         if device is None:
             try:
@@ -280,7 +280,7 @@ class Net(keras.Sequential):
             Scaled sources
         '''
         for sample in range(source.shape[0]):
-            source[sample] /= source[sample].std()
+            source[sample] /= np.max(np.abs(source[sample]))
 
         return source
             
@@ -342,11 +342,12 @@ class Net(keras.Sequential):
         if self.temporal:
             eeg_prep = np.swapaxes(eeg_prep, 1,2)
         predicted_sources = self.predict_sources(eeg_prep)       
-        
+        print(f'predicted_sources min: {predicted_sources.min()},predicted_sources max: {predicted_sources.max()}')
         
         # Rescale Predicitons
         # predicted_sources_scaled = self._solve_p_wrap(predicted_sources, eeg)
         predicted_sources_scaled = self._scale_p_wrap(predicted_sources, eeg)
+        print(f'rescaled: predicted_sources min: {predicted_sources.min()},predicted_sources max: {predicted_sources.max()}')
 
         # Convert sources (numpy.ndarrays) to mne.SourceEstimates objects
         if predicted_sources.shape[-1] == 1:
@@ -507,8 +508,9 @@ class Net(keras.Sequential):
         self.add(layers.LSTM(self.n_lstm_units, return_sequences=True, 
             input_shape=(self.n_timepoints, self.n_channels)))
         self.add(layers.Flatten())
-        self.add(layers.Dense(int(self.n_timepoints*self.n_dipoles), activation=self.activation_function))
+        self.add(layers.Dense(int(self.n_timepoints*self.n_dipoles), activation='linear'))
         self.add(layers.Reshape((self.n_timepoints, self.n_dipoles)))
+        self.add(layers.Activation('linear'))
         
         self.build(input_shape=input_shape)
         
@@ -522,7 +524,7 @@ class Net(keras.Sequential):
                                 activation=self.activation_function))
         # Add output layer
         self.add(layers.Dense(self.n_dipoles, 
-            activation=keras.layers.ReLU()))
+            activation='linear'))
         
         # Build model with input layer
         self.build(input_shape=(None, self.n_channels))

ESINet/simulation.py
@@ -16,7 +16,7 @@ DEFAULT_SETTINGS = {
             'duration_of_trial': 0,
             'sample_frequency': 100,
             'target_snr': 4,
-            'beta': (0, 1.5),
+            'beta': (0, 3),
         }
 
 class Simulation:
@@ -204,17 +204,11 @@ class Simulation:
             signals = []
             for _ in range(number_of_sources):
                 signal = cn.powerlaw_psd_gaussian(self.get_from_range(self.settings['beta'], dtype=float), signal_length) 
-                signal += np.abs(np.min(signal))
-                signal /= np.max(signal)
+                # Old: have positive source values
+                # signal += np.abs(np.min(signal))
+                # signal /= np.max(signal)
+                signal /= np.max(np.abs(signal))
                 signals.append(signal)
-
-
-            # signal = np.zeros((signal_length))
-            # start = int(np.floor((signal_length - pulselen) / 2))
-            # end = int(np.ceil((signal_length - pulselen) / 2))
-            # signal[start:-end] = pulse
-            # signal += np.min(signal)
-            # signal /= np.max(signal)
             
             sample_frequency = self.settings['sample_frequency']
         else:  # else its a single instance
@@ -285,7 +279,7 @@ class Simulation:
                 list of either mne.Epochs objects or list of raw EEG data 
                 (see argument <return_raw_data> to change output)
         '''
-        t_start = time()
+
         n_simulation_trials = 20
          
         # Desired Dim of sources: (samples x dipoles x time points)
@@ -302,15 +296,13 @@ class Simulation:
 
         # Load some forward model objects
         fwd_fixed, leadfield = util.unpack_fwd(self.fwd)[:2]
-        n_samples, n_dipoles, n_timepoints = sources.shape
+        n_samples, _, _ = sources.shape
         n_elec = leadfield.shape[0]
 
-        start_project = time()
         
         # Desired Dim for eeg_clean: (samples, electrodes, time points)
         eeg_clean = self.project_sources(sources)
 
-        t_function_setup = time()
         if self.verbose:
             print(f'\nCreate EEG trials with noise...')
         if self.parallel: