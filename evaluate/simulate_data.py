9ea219c60f80be46794db70854a816fbc3b873e1
LukeTheHecker <lukas_hecker@web.de>
2022-01-22 17:20:05 +0100

    to overwrite

--- evaluate/simulate_data.py
+++ evaluate/simulate_data.py
@@ -24,10 +24,10 @@ fwd_free = forward.create_forward_model(info=info, fixed_ori=False)
 # Create Data set
 n_samples = 10000
 duration_of_trial = (0.01, 2)
-method = 'noise'
-exponent = 3
+method = 'standard'
+extents = 1
 
-settings = dict(duration_of_trial=duration_of_trial, method=method)
+settings = dict(duration_of_trial=duration_of_trial, method=method, extents=extents, )
 sim_short = Simulation(fwd, info, verbose=True, settings=settings).simulate(n_samples=n_samples)
 
 n_samples = 200
@@ -40,6 +40,6 @@ sim = sim_short + sim_long
 del sim_short, sim_long
 sim.shuffle()
 
-sim.save(f'simulations/sim_{sim.n_samples}_1-1000points_noise.pkl')
+sim.save(f'simulations/sim_{sim.n_samples}_1-1000points_single-dipoles_standard.pkl')
 
 ########################################################################
